8bace177d7bd3f8c89b87f9d2c1bdfd81503e74e
Tr8demark <kennethlivermore66@gmail.com>
2022-08-21 11:07:20 -0500

    Pseudocode add and add idea.

--- Python/uiCalc.py
+++ Python/uiCalc.py
@@ -1,11 +1,18 @@
+__name__ = '__main__'
+
 ###########################################################
 """
 Author: Kenneth Livermore aka Tr8demark
 Date: 08/22/22
 Title: UICalc
-Language: Python
+Language: Python3
 """
 ###########################################################
+"""
+Pseudocode:
+A field that updates when numbers are entered at the top.
+A 3x5 grid of numbers and operands that can be selected by keypress or mouse.
+Probably just use the top field as a python wrapper.
+"""
 
-__name__ = '__main__'
-
+import PySimpleGUI as sg
