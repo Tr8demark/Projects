07033f3da81d0ad2e0c17183b49323636f8c9a82
Tr8demark <kennethlivermore66@gmail.com>
2022-08-21 13:09:41 -0500

    Rough grid organization, no logic

--- Python/uiCalc.py
+++ Python/uiCalc.py
@@ -16,3 +16,20 @@ Probably just use the top field as a python wrapper.
 """
 
 import PySimpleGUI as sg
+#Manual: https://www.pysimplegui.org/en/latest/
+
+layout = [
+    [sg.Button("/"), sg.Button("*"), sg.Button("-")],
+    [sg.Button("7"), sg.Button("8"), sg.Button("9")],
+    [sg.Button("4"), sg.Button("5"), sg.Button("6")],
+    [sg.Button("1"), sg.Button("2"), sg.Button("3")],
+    [sg.Button("0"), sg.Button("."), sg.Button("+")],
+    [sg.Output(size = (10, 5))]
+    ]
+
+window = sg.Window('File Compare', layout)
+while True:                             # The Event Loop
+    event, values = window.read()
+    # print(event, values) #debug
+    if event in (None, 'Exit', 'Cancel'):
+        break
