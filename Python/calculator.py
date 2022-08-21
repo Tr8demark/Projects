43d85868d05a627be2bec4a9435a77a570a0632a
Tr8demark <kennethlivermore66@gmail.com>
2022-08-20 23:00:25 -0500

    Removed old code and created if/elif/else lines.

--- Python/calculator.py
+++ Python/calculator.py
@@ -1,61 +1,5 @@
 import math
 
-"""
-PseudoCode:
-    If interactive (with UI):
-        Buttons laid out in same style as 10-key on keyboard.
-    If terminal-only:
-        Request how many inputs it will need.
-        Request what function they are needing between each input.
-        Put each input in an array/list and do the same for the operands.
-        Perform calculation by putting the inputs and operands in order.
-
-Will do terminal-only first
-"""
-
-"""
-##With if/elif statements
-
-i = int(1)
-while i < inputs:
-    if i == 1:
-        first = input(str(i)+"st number: ")
-        i = i + 1
-    elif i == 2:
-        second = input(str(i) + "nd number: ")
-        i = i + 1
-    elif i == 3:
-        third = input(str(i) + "rd number: ")
-        i = i + 1
-    else:
-        i = input(str(i) + "th number: ")
-        i = i + 1
-"""
-
-"""
-##With Lists
-
-inputs = int(input("How many are you needing? "))
-print(inputs)
-values = []
-operands = []
-
-i = 0
-while i < int(inputs):
-    if i  == 0:
-        values.append(int(input("What is the First number? ")))
-        print(values[i])
-        print(i)
-        i =+ 1
-    elif i == 1: ##BUG: Repeats This section; Jumps from 0 to 2.
-        values.append(int(input("What is the Second number? ")))
-        print(values[i])
-        i = i + 1
-        print(i) 
-    else:
-        print(values)
-"""
-
 ##With Dictionary
 print("""
 1. add
@@ -67,12 +11,9 @@ print("""
 #Add in error handling for invalid inputs. 
 
 inputs = input("What function are you needing to perform? ")
-inputs = inputs.lower
+inputs = inputs.lower()
 
-
-f_num = int(input("First number: "))
-s_num = int(input("Second number: "))
-
+### Defining functions
 def add(f_num, s_num):
     __doc__ = "Add two numbers"
     return f_num + s_num
@@ -86,10 +27,31 @@ def div(f_num, s_num):
     __doc__ = "Divide two numbers showing only a whole number"
     return f_num // s_num
 
-mathChoices = {
-    "add": add(f_num, s_num),
-    "sub": sub(f_num, s_num),
-    "multi": multi(f_num, s_num),
-    "div": div(f_num, s_num)
-}
-print(mathChoices[inputs])
+if inputs == "add" or "sub" or "multi" or "div":
+    
+    f_num = int(input("First number: "))
+    s_num = int(input("Second number: "))
+
+    mathChoices = {
+        "add": add(f_num, s_num),
+        "sub": sub(f_num, s_num),
+        "multi": multi(f_num, s_num),
+        "div": div(f_num, s_num)
+    }
+    print(mathChoices[inputs])
+
+elif inputs ==  1 or 2 or 3 or 4:
+    inputs = int(inputs)
+    f_num = int(input("First number: "))
+    s_num = int(input("Second number: "))
+
+    mathChoices = {
+        "add": add(f_num, s_num),
+        "sub": sub(f_num, s_num),
+        "multi": multi(f_num, s_num),
+        "div": div(f_num, s_num)
+    }
+
+    print(mathChoices[inputs])
+else:
+    print("You have not provided a valid input, please run the program, again.")
