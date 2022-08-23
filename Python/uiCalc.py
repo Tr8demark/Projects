ed2d5ce0fac6f85033d3cef9cc23addee4aaf1ab
Tr8demark <kennethlivermore66@gmail.com>
2022-08-23 08:43:31 -0500

    Modified original layout and added menu skeleton

--- Python/uiCalc.py
+++ Python/uiCalc.py
@@ -15,21 +15,38 @@ A 3x5 grid of numbers and operands that can be selected by keypress or mouse.
 Probably just use the top field as a python wrapper.
 """
 
+from typing import Any
 import PySimpleGUI as sg
 #Manual: https://www.pysimplegui.org/en/latest/
 
-layout = [
-    [sg.Button("/"), sg.Button("*"), sg.Button("-")],
-    [sg.Button("7"), sg.Button("8"), sg.Button("9")],
-    [sg.Button("4"), sg.Button("5"), sg.Button("6")],
-    [sg.Button("1"), sg.Button("2"), sg.Button("3")],
-    [sg.Button("0"), sg.Button("."), sg.Button("+")],
-    [sg.Output(size = (10, 5))]
+sg.theme('DarkAmber')
+
+calcLayout = [
+    #Menu Example: [[sg.MenubarCustom([['File', ['Exit']], ['Edit', ['Edit Me', ]]])]],
+    [sg.MenubarCustom([['File', ["Settings", "Exit"]], ['History']])],
+    [sg.Input(size = (15, 5)), sg.Button("Calculate")],
+    [sg.Button(" /  "), sg.Text("     "), sg.Button(" * "), sg.Text("     "), sg.Button("  - ")],
+    [sg.Button(" 7 "), sg.Text("     "), sg.Button(" 8 "),sg.Text("     "), sg.Button(" 9 ")],
+    [sg.Button(" 4 "), sg.Text("     "), sg.Button(" 5 "), sg.Text("     "), sg.Button(" 6 ")],
+    [sg.Button(" 1 "), sg.Text("     "), sg.Button(" 2 "), sg.Text("     "), sg.Button(" 3 ")],
+    [sg.Button(" 0 "), sg.Text("     "), sg.Button(" .  "), sg.Text("     "), sg.Button(" + ")],
+    [sg.Output(size=(20,1), sbar_width=None)]
     ]
 
-window = sg.Window('File Compare', layout)
+settLayout = [
+    [sg.MenubarCustom(['Save and Exit'], ['Exit'])]
+]
+
+histLayout = [
+    [sg.MenubarCustom(['Exit'])]
+]
+
+calcWin = sg.Window('Calculator', calcLayout)
+settWin = sg.Window('Settings', settLayout)
+histWin = sg.Window('History', histLayout)
+
 while True:                             # The Event Loop
-    event, values = window.read()
+    event, values = calcWin.read()
     # print(event, values) #debug
     if event in (None, 'Exit', 'Cancel'):
         break
