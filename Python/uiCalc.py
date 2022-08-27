544298ed2e77364182cea94d81ddafcb86ec2062
Tr8demark <kennethlivermore66@gmail.com>
2022-08-27 15:43:16 -0500

    Button formatting, Titlebar add. AboutLayout add.

--- Python/uiCalc.py
+++ Python/uiCalc.py
@@ -12,41 +12,56 @@ Language: Python3
 Pseudocode:
 A field that updates when numbers are entered at the top.
 A 3x5 grid of numbers and operands that can be selected by keypress or mouse.
-Probably just use the top field as a python wrapper.
 """
 
+import webbrowser as wb
 import PySimpleGUI as sg
 #Manual: https://www.pysimplegui.org/en/latest/
 
 sg.theme('DarkAmber')
-
+history = ["List:","Test"]
 calcLayout = [
+    [sg.Titlebar('Calulator')],
     #Menu Example: [[sg.MenubarCustom([['File', ['Exit']], ['Edit', ['Edit Me', ]]])]],
     [sg.MenubarCustom(
-        [['File', ["Settings", "Exit",]],
-        ['Help', ["About...",]]])],
+        [['File', ["Settings", "Exit", "History"]],
+        ['Help', ["About...",]],
+        ['History'],])],
     [sg.Input(size = (15, 5), k = '-INPUT-'), sg.Button("Calculate", k = '-CALC-'), sg.Button("History", k = '-HIST-')],
-    [sg.Button(" / ", k = '-DIV-'), sg.Text("      "), sg.Button(" x ", k = '-MULTI-'), sg.Text("     "), sg.Button(" -  ", k = ' -SUB-')],
-    [sg.Button(" 7 ", k = '-7-'), sg.Text("     "), sg.Button(" 8 ", k = '-8-'),sg.Text("     "), sg.Button(" 9 ", k = '-9-')],
-    [sg.Button(" 4 ", k = '-4-'), sg.Text("     "), sg.Button(" 5 ", k = '-5-'), sg.Text("     "), sg.Button(" 6 ", k = '-6-')],
-    [sg.Button(" 1 ", k = '-1-'), sg.Text("     "), sg.Button(" 2 ", k = '-2-'), sg.Text("     "), sg.Button(" 3 ", k = '-3-')],
-    [sg.Button(" 0 ", k = '-0-'), sg.Text("     "), sg.Button(" .  ", k = '-DOT-'), sg.Text("     "), sg.Button(" + ", k = '-ADD-')],
-    [sg.Output(size=(20,1), sbar_width=None)]
+    [sg.Button("/", k = '-DIV-', size = (2,1)), sg.Text("     "), sg.Button("x", k = '-MULTI-', size = (2,1)), sg.Text("     "), sg.Button("-", k = ' -SUB-', size = (2,1))],
+    [sg.Button("7", k = '-7-', size = (2,1)), sg.Text("     "), sg.Button("8", k = '-8-', size = (2,1)),sg.Text("     "), sg.Button("9", k = '-9-', size = (2,1))],
+    [sg.Button("4", k = '-4-', size = (2,1)), sg.Text("     "), sg.Button("5", k = '-5-', size = (2,1)), sg.Text("     "), sg.Button("6", k = '-6-', size = (2,1))],
+    [sg.Button("1", k = '-1-', size = (2,1)), sg.Text("     "), sg.Button("2", k = '-2-', size = (2,1)), sg.Text("     "), sg.Button("3", k = '-3-', size = (2,1))],
+    [sg.Button("0", k = '-0-', size = (2,1)), sg.Text("     "), sg.Button(".", k = '-DOT-', size = (2,1)), sg.Text("     "), sg.Button("+", k = '-ADD-', size = (2,1))],
+    [sg.Output(size=(20,1), sbar_width = 0)]
     ]
 
 settLayout = [
+    [sg.Titlebar('Settings')],
     [sg.MenubarCustom(
-        [["File", ['Save and Exit'], ['Exit']]])]
+        [["File", ['Save and Exit', 'Exit']]])],
+        [sg.DropDown([])]
 ]
 
 histLayout = [
+    [sg.Titlebar('History')],
     [sg.MenubarCustom(
-        [["File", ["Exit"],["Test"]]])]
+        [["File", ["Exit", "Test"]]])],
+        [sg.Listbox(history, size=(max(map(len, history))+2, 10), key='HISTLIST')]
+]
+
+aboutLayout = [
+    [sg.Titlebar('About')],
+    [sg.Text("Created by Tr8demark")],
+    [sg.Text("Version: 0.1")],
+    [sg.Button("Github", enable_events = True)]
 ]
 
 calcWin = sg.Window('Calculator', calcLayout)
 settWin = sg.Window('Settings', settLayout)
 histWin = sg.Window('History', histLayout)
+aboutWin = sg.Window('About', aboutLayout)
+url = 'https://github.com/Tr8demark/Projects/blob/main/Python/uiCalc.py'
 
 while True:                             # The Event Loop
     event, values = calcWin.read()
@@ -55,9 +70,14 @@ while True:                             # The Event Loop
         histWin.read()
     if event == 'Settings':
         settWin.read()
+    if event == 'About...':
+        aboutWin.read()
+    if event == "Github":
+        wb.open(url, new = 0, ),
+        print("Test")
 
     if event == '-CALC-':
-        #push the result of the input to the output
+        #push the result of the input to the output and append string to history list
         pass
     if event == '-DIV-':
         #enter / into the input field
