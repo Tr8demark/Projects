27c8372b024f89e2b980820f86411e3703540002
Tr8demark <kennethlivermore66@gmail.com>
2022-09-01 21:04:10 -0500

    Dropdown Theme shell. Clickable text T-shooting.

--- Python/uiCalc.py
+++ Python/uiCalc.py
@@ -24,7 +24,7 @@ calcLayout = [
     [sg.Titlebar('Calulator')],
     #Menu Example: [[sg.MenubarCustom([['File', ['Exit']], ['Edit', ['Edit Me', ]]])]],
     [sg.MenubarCustom(
-        [['File', ["Settings", "Exit", "History"]],
+        [['File', ["Settings", "Exit"]],
         ['Help', ["About...", "Github"]],
         ['History', []]])],
     [sg.Input(size = (15, 5), k = '-INPUT-'), sg.Button("Calculate", k = '-CALC-')],
@@ -33,39 +33,40 @@ calcLayout = [
     [sg.Button("4", k = '-4-', size = (2,1)), sg.Text("     "), sg.Button("5", k = '-5-', size = (2,1)), sg.Text("     "), sg.Button("6", k = '-6-', size = (2,1))],
     [sg.Button("1", k = '-1-', size = (2,1)), sg.Text("     "), sg.Button("2", k = '-2-', size = (2,1)), sg.Text("     "), sg.Button("3", k = '-3-', size = (2,1))],
     [sg.Button("0", k = '-0-', size = (2,1)), sg.Text("     "), sg.Button(".", k = '-DOT-', size = (2,1)), sg.Text("     "), sg.Button("+", k = '-ADD-', size = (2,1))],
-    [sg.Output(size=(20,1), sbar_width = 0)]
+    [sg.Output(size=(20,1), sbar_width = 0)],
+    [sg.Text("Github", size = 2, key = "git")]
     ]
 
 settLayout = [
     [sg.Titlebar('Settings')],
     [sg.MenubarCustom(
         [["File", ['Save and Exit', 'Exit']]])],
-        [sg.DropDown([])]
+        [sg.Text("Theme: "), sg.DropDown(["Default"])]
 ]
 
 histLayout = [
     [sg.Titlebar('History')],
     [sg.MenubarCustom(
         [["File", ["Exit"]]])],
-        [sg.Listbox(history, size=(max(map(len, history))+2, 10), key='HISTLIST')]
+    [sg.Listbox(history, size=(max(map(len, history))+2, 10), key='HISTLIST')]
 ]
 
 aboutLayout = [
     [sg.Titlebar('About')],
     [sg.Text("Created by Tr8demark")],
     [sg.Text("Version: 0.1")],
-    [sg.Text("Github", enable_events = True, k = "textgit")]
+    [sg.Text("Github", click_submits = True, key = "textgit")]
 ]
 
-calcWin = sg.Window('Calculator', calcLayout)
+calcWin = sg.FlexForm('Calculator').Layout(calcLayout)
 settWin = sg.Window('Settings', settLayout)
 histWin = sg.Window('History', histLayout)
-aboutWin = sg.Window('About', aboutLayout)
+aboutWin = sg.FlexForm('About').Layout(aboutLayout)
 urlgit = "https://github.com/Tr8demark/Projects/blob/main/Python/uiCalc.py"
 
 while True:                         # The Event Loop
     event, values = calcWin.read()
-     #print(event, values) #debug
+    print(event, values) #debug
 #   Read Windows
     if event == 'History':
         histWin.read()
@@ -77,48 +78,48 @@ while True:                         # The Event Loop
 #    if event == "textgit":
 #        wb.open(urlgit, new = 0, autoraise = True)
 #    Below works; above does not work. Clickable text issues.
-    if event == "Github":
-        wb.open(urlgit, new = 0, autoraise = True)
+    if event == "textgit":
+        wb.open(urlgit, new = 1, autoraise = True)
 
 
     if event == '-CALC-':
         #push the result of the input to the output and append string to history list
-        pass
+        None
     if event == '-DIV-':
         #enter / into the input field
-        pass
+        None
     if event == '-MULTI-':
         #enter * character into the input field
-        pass
+        None
     if event == '-SUB-':
         #enter - character into input field
-        pass
+        None
     if event == '-DOT-':
         #enter . character into the input field
-        pass
+        None
     if event == '-ADD-':
         #enter + character into the input field
-        pass
+        None
     if event == '-9-':
-        pass
+        None
     if event == '-8-':
-        pass
+        None
     if event == '-7-':
-        pass
+        None
     if event == '-6-':
-        pass
+        None
     if event == '-5-':
-        pass
+        None
     if event == '-4-':
-        pass
+        None
     if event == '-3-':
-        pass
+        None
     if event == '-2-':
-        pass
+        None
     if event == '-1-':
-        pass
+        None
     if event == '-0-':
-        pass
+        None
 
     if event in (None, 'Exit', 'Cancel'):
         break
